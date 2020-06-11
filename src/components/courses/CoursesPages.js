ade68dcb85b2fdf27d7dc628e11f03aae7c06fc5
Joao Carneiro Xavier <Joao_Carneiro_Xavier@ncci.com>
2020-06-11 13:54:02 -0400

    Initial commit

--- src/components/courses/CoursesPages.js
+++ src/components/courses/CoursesPages.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CoursesPages = () => {
-  return <h2>Courses</h2>;
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(title);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Courses</h3>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <input type="submit" value="save" />
+    </form>
+  );
 };
 export default CoursesPages;
